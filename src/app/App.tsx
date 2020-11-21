35a391cea2bfd91d349cad0c4e97693ac3b574fd
Almaz <atunstyle@gmail.com>
2020-11-21 17:26:50 +0300

    init commit :tada:

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <span>hello</span>
+        </div>
+    );
+}
+
+export default App;
