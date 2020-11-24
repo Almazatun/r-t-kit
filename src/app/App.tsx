b5074e6fbc54333964aeaf4dfd141ce2a1bd67a7
Almaz <atunstyle@gmail.com>
2020-11-24 12:07:24 +0300

    :recycle:refactor

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
 import {Scoreboard} from "../features/Scoreboard";
+import {Navbar} from "../components/Navbar/Navbar";
+import {Route, Switch} from "react-router-dom";
 
 const App: React.FC = () => {
-    return  <Scoreboard/>
+
+    return (
+        <div>
+            <Navbar/>
+            <Switch>
+                <Route path={"/scoreboard"} render={() => <Scoreboard/>}/>
+            </Switch>
+        </div>
+    )
 }
 
 export default App;
